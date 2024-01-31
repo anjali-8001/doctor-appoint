3f8bc228353a277895adf84a31b0d5ac6be24b5f
anjali-8001 <93631333+anjali-8001@users.noreply.github.com>
2024-01-31 11:12:50 +0530

    made changes for deplyment

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "client/build/index.html "));
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 const port = process.env.PORT || 8000;
