a1aa3be4d9ecf806f2d1d3f7a4f1c63daa3c85f0
anjali-8001 <anjalisharma8001@gmail.com>
2024-04-23 07:33:42 +0530

    make changes in model

--- models/userModels.js
+++ models/userModels.js
@@ -1,35 +1,38 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "name is required"],
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isDoctor: {
+      type: Boolean,
+      default: false,
+    },
+    notification: {
+      type: Array,
+      default: [],
+    },
+    seennotification: {
+      type: Array,
+      default: [],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "email is required"],
-  },
-  password: {
-    type: String,
-    required: [true, "password is required"],
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-  isDoctor: {
-    type: Boolean,
-    default: false,
-  },
-  notification: {
-    type: Array,
-    default: [],
-  },
-  seennotification: {
-    type: Array,
-    default: [],
-  },
-});
+  { timestamps: true }
+);
 
 const userModel = mongoose.model("users", userSchema);
 
